b0aca20a2dbbfc13428c219e1c3d3337686b9ad6
Docie <eudocia.ng@gmail.com>
2022-07-11 16:42:43 +1200

    install lightgallery

--- client/views/user/Gardens/Gallery/Gallery.jsx
+++ client/views/user/Gardens/Gallery/Gallery.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import lightGallery from 'lightgallery'
 
 function Gallery() {
   return (
     <>
-      <main>
+      <div id="lightgallery">
         <p> hi, this is the gallery</p>
-        <img src="/images/comGardenPlant.png" alt="garden image1" />
-        <img src="/images/comGardenRows.png" alt="garden image2" />
-      </main>
+        <a href="/images/comGardenPlant.png" data-lg-size="1600-2400">
+          <img alt="garden image1" src="/images/comGardenPlant.png" />
+        </a>
+        <a href="/images/comGardenRows.png" data-lg-size="1600-2400">
+          <img alt="garden image2" src="/images/comGardenRows.png" />
+        </a>
+        {/* <img src="/images/comGardenPlant.png" alt="garden image1" /> */}
+        {/* <img src="/images/comGardenRows.png" alt="garden image2" /> */}
+      </div>
     </>
   )
 }
